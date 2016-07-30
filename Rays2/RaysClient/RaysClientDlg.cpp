28174b4313badfeba051e84c24304b6a81b5c0da
F072335 <fabrice.ferrand@sagem.com>
2016-07-30 22:06:58 +0200

    Ajout parsing Raptor xml a RaysClient

--- Rays2/RaysClient/RaysClientDlg.cpp
+++ Rays2/RaysClient/RaysClientDlg.cpp
@@ -1,7 +1,8 @@
 #include "stdafx.h"
-#include <vcclr.h>
+//#include <vcclr.h>
 #include <windows.h>
 #include "RaysClientDlg.h"
+#include "RaysClientData.h"
 #include "Subsys/CodeGeneration.h"
 
 #if !defined(AFX_CMDLINEPARSER_H__D7D8768A_3D97_491F_8493_588972A3CF62__INCLUDED_)
@@ -11,7 +12,7 @@
 using namespace RaysClient;
 
 RaysClientDlg::RaysClientDlg()
-	:m_started(false)
+	:m_started(false), m_pData(NULL)
 {
 #ifdef WIN32
 	Network::setNbConnectAttempts(1);
@@ -22,6 +23,7 @@ RaysClientDlg::RaysClientDlg()
 	RaysClientUtils::setLog(gcnew RaysClientDlg::RaysLogger(this));
 
 	m_raysClient = new CClient<CClientSocket>();
+	m_pData = new RaysClientData();
 	AddLog("Rays Client initialized.");
 }
 
@@ -29,6 +31,8 @@ RaysClientDlg::~RaysClientDlg()
 {
 	if (NULL != m_raysClient)
 		delete m_raysClient;
+	if (NULL != m_pData)
+		delete m_pData;
 }
 
 bool RaysClientDlg::Start(int argc,char *argv[])
@@ -69,4 +73,14 @@ bool RaysClientDlg::Start(int argc,char *argv[])
 bool RaysClientDlg::Quit(void)
 {
 	return !m_started;
+}
+
+bool RaysClientDlg::LoadData(String^ filename)
+{
+	AddLog("Loading: "+filename);
+
+	m_pData->initDataloader();
+	m_pData->LoadData(RaysClientUtils::convertSystemString(filename));
+
+	return true;
 }
