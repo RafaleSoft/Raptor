e55818b7e71888328fd371b25337e8050c2c4415
Fabrice FERRAND <fabfer@wanadoo.fr>
2017-12-02 16:13:55 +0100

    Add files via upload

---
+++ VC++ 2008.bat
@@ -0,0 +1,45 @@
+set RAPTOR_VERSION="2.16.6"
+set RAPTOR_ROOT=E:\Share\Raptor
+set PATH=%PATH%;E:\Share\Raptor\Redist\bin
+set WMSDK=E:\WMSDK\WMFSDK95
+set PLATFORM_SDK=E:\Microsoft SDKs v7.0
+
+set FREETYPE_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\freetype-2.5.5\include
+set FREETYPE_LIB_PATH=%RAPTOR_ROOT%\AddOns\freetype-2.5.5\lib
+set FREETYPE_BIN_PATH=%RAPTOR_ROOT%\AddOns\freetype-2.5.5\bin
+
+set ZLIB_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\zlib128\include
+set ZLIB_LIB_PATH=%RAPTOR_ROOT%\AddOns\zlib128\lib\VC9
+set ZLIB_BIN_PATH=%RAPTOR_ROOT%\AddOns\zlib128\bin\VC9
+
+set XERCES_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\xercesc_2_7
+set XERCES_LIB_PATH=%RAPTOR_ROOT%\AddOns\xercesc_2_7\vc9
+set XERCES_BIN_PATH=%RAPTOR_ROOT%\AddOns\xercesc_2_7\vc9
+
+set JPEGLIB_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\JpegLib\include
+set JPEGLIB_LIB_PATH=%RAPTOR_ROOT%\AddOns\JpegLib\lib
+set JPEGLIB_BIN_PATH=%RAPTOR_ROOT%\AddOns\JpegLib\bin
+
+set TIFFLIB_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\TiffLib\include
+set TIFFLIB_LIB_PATH=%RAPTOR_ROOT%\AddOns\TiffLib\lib\vc9
+set TIFFLIB_BIN_PATH=%RAPTOR_ROOT%\AddOns\TiffLib\bin\vc9
+
+set PNGLIB_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\libpng-1.6.16\include
+set PNGLIB_LIB_PATH=%RAPTOR_ROOT%\AddOns\libpng-1.6.16\lib\VC9
+set PNGLIB_BIN_PATH=%RAPTOR_ROOT%\AddOns\libpng-1.6.16\bin\VC9
+
+set OPENEXRLIB_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\OpenEXRLib\include
+set OPENEXRLIB_LIB_PATH=%RAPTOR_ROOT%\AddOns\OpenEXRLib\lib\vc9
+set OPENEXRLIB_BIN_PATH=%RAPTOR_ROOT%\AddOns\OpenEXRLib\bin\vc9
+
+set OPENCL_INCLUDE_PATH=C:\Program Files (x86)\Intel\OpenCL SDK\2.0\include
+set OPENCL_LIB_PATH=C:\Program Files (x86)\Intel\OpenCL SDK\2.0\lib\x86
+set OPENCL_BIN_PATH=C:\Program Files (x86)\Intel\OpenCL SDK\2.0\bin\x86
+
+set VULKAN_INCLUDE_PATH=%RAPTOR_ROOT%\AddOns\vulkan\include
+set VULKAN_BIN_PATH=c:\windows\system32
+
+cd %RAPTOR_ROOT%
+
+set MSSdk=E:\Microsoft Platform SDK for Windows Server 2003 R2
+"E:\Microsoft Visual Studio 9.0\Common7\IDE\VCExpress.exe"
