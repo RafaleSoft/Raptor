a4eb83d65b62f79c9c83b0b034951c78cdecea3e
F072335 <fabrice.ferrand@safrangroup.com>
2019-08-17 10:56:09 +0200

    Migration vers Builder C# et Suppression Builder.Net

--- Builder/Form1.Designer.cs
+++ Builder/Form1.Designer.cs
@@ -1,4 +1,22 @@
-﻿namespace Builder
+﻿/***************************************************************************/
+/*                                                                         */
+/*  Form1.designer.cs                                                      */
+/*                                                                         */
+/*    Raptor OpenGL & Vulkan realtime 3D Engine SDK.                       */
+/*                                                                         */
+/*  Copyright 1998-2019 by                                                 */
+/*  Fabrice FERRAND.                                                       */
+/*                                                                         */
+/*  This file is part of the Raptor project, and may only be used,         */
+/*  modified, and distributed under the terms of the Raptor project        */
+/*  license, LICENSE.  By continuing to use, modify, or distribute         */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+namespace Builder
 {
     partial class Form1
     {
