f25bf6fd88fe4c14de28eb397c1d2afc6aebf1cf
FERRAND Fabrice (SAFRAN ELECTRONICS F072335 DEFENSE) <F072335@sds.safran>
2019-12-20 18:28:52 +0100

    Création Rays Client CSharp

---
+++ Rays2/RaysClient/RaysClientForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaysClient
+{
+    public partial class RaysClientForm : Form
+    {
+        public RaysClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RaysClientForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
